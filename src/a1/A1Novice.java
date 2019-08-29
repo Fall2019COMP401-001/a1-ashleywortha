ac648440981a4fadffe24718b44c532512e65e55
Ashley <Ashley@152.23.41.59>
2019-08-29 17:38:48 -0400

    Completed A1 Novice

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -9,6 +9,40 @@ public class A1Novice {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
+		// Read and count the number of customers
+		int count = scan.nextInt();
 		
+		// Create arrays that stores customers info
+		char[] name = new char[count];
+		String[] last = new String [count];
+		double[] totalCost = new double[count];
+		//Read customer info into arrays
+		for (int i = 0; i < count; i++) {
+			// array for first letter of first name
+			name[i] = scan.next().charAt(0);
+			// array for last name
+			last[i] = scan.next();
+			// count the total number of items customer has
+			int totalItems = scan.nextInt();
+			double total = 0;
+			for (int k = 0; k < totalItems; k++) {
+				   // number of x item
+				   int numberOfItem = scan.nextInt();
+				   // name of item
+				   String itemName = scan.next();
+				   // cost of x item
+				   Float costOfItem = scan.nextFloat();
+				   total += numberOfItem * costOfItem;
+				   }	
+			totalCost[i] = total;
+		}
+		     
+		// all input entered, so close scanner
+		scan.close();
+		// print results looped in 
+		for (int j = 0; j < count; j++) {
+		System.out.println(name[j] + ". " + last[j]+ ": " + String.format("%.2f",totalCost[j]));
+		}
 	}
+	
 }
